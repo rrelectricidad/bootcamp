da2912b17c8a25a1be2207d0151b6853b27f1508
ruben riveros <ruben.rrlm54@gmail.com>
2019-07-16 12:08:53 -0400

    nuevo archivo agreagdo

---
+++ dia3.py
@@ -0,0 +1,93 @@
+#############condicionales#############
+
+
+# == igual
+# > mayor que
+#< menor que
+#>= mayor igual
+#<= menor igual
+# ! distinto de
+############################### if   else   elif  ################################
+
+# edad = 7
+# if edad > 18:
+#     print("deberia estar en la universidad")
+# elif edad > 13:
+#     print("deveria estar en la secundaria")
+# elif edad > 6:
+#     print("deberia esatr en primaria")
+# else:
+#     print("deveria estar en casa bbdlc")
+    
+################################## anidad##############################################
+
+# if edad > 18:
+#     print("universidad")
+# else:
+#     if edad > 13:
+#         print("secundaria")
+#     else:
+#         if edad > 6 :
+#             print("primaria")
+#         else:
+#             print("bbdlc")
+
+
+
+####################################### ejercicio###############################
+
+
+############################ input####################################################
+
+# pedir al usuario que ingrese tres numeros y multiplicarlos
+
+# entre si, imprimir el rultad
+
+# juan = input("primero")
+# fabio = input("segundo")
+# rr = input("tercero")
+# suma = int(juan) * int(fabio) * int(rr)
+# print(suma)
+
+################## primera prueba lograda##############
+
+
+
+###################### bucle while    == mientras##################################
+
+# x = 8
+# while x != 5:
+#     print("esto es while")
+#     x = int(input("ingresa"))
+#     print("el valor de x ", x)
+# print("termino")
+
+######################### estoy muerto :)##########################################
+# Ej. crear un juego de 
+
+# varia = False 
+# numero_secreto = 69
+# while varia == False:
+#     apuesta = input("adivina el numero secreto , del 1 al 100") 
+#     if int(apuesta) == numero_secreto:
+#         print("ganasteeeee!!!!!!!!")
+#         varia = True
+#     elif int(apuesta) = < 69 numero mayor:
+#     else:
+#         print("apuesta")
+        
+    
+
+
+        ################# braboooooooooooo#####################
+
+             
+
+
+
+
+     
+    
+
+    
+

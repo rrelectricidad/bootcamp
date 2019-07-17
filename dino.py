7f0a445bfbcf97d81c0b33c5fa5d5a4dd8dc9e0c
ruben riveros <ruben.rrlm54@gmail.com>
2019-07-17 15:08:13 -0400

    archivo orientacion de objetos

---
+++ dino.py
@@ -0,0 +1,19 @@
+class Dino: 
+    def __init__(self,un_nombre , una_edad , altura):
+        self.nombre = un_nombre
+        self.edad = una_edad
+        self.altur = altura
+        print("que hay")
+
+    def saludar(self):
+        print("hola me llamo", self.nombre, "tengo", self.edad, "ano y mido", self.altur, "metros")
+    
+    # def cortar_altur(sel):
+    #     self.altura = self.altur - cntidad_de_altur_a_cortar
+
+
+
+vari = Dino( "tirano", 1, "20")
+
+
+vari.saludar()
